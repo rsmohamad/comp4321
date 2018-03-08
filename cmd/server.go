e3e540f19d7d65164468d27a50b556c208d8cb88
Dandi <rsmohamad@ust.hk>
2018-03-08 23:15:33 +0800

    Add simple result page

--- cmd/server.go
+++ cmd/server.go
@@ -4,31 +4,51 @@ import (
 	"fmt"
 	"net/http"
 	"html/template"
+	"comp4321/database"
+	"comp4321/models"
 )
 
-var t = template.Must(template.ParseFiles("views/home.html"))
+var homeTemplate = template.Must(template.ParseFiles("views/home.html"))
+var resultTemplate = template.Must(template.ParseFiles("views/results.html"))
+var indexer *database.Indexer
 
-func helloWorldHandler (w http.ResponseWriter, r *http.Request){
-	t.Execute(w, nil)
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	homeTemplate.Execute(w, nil)
 	fmt.Println(r.URL.String())
 }
 
-func searchHandler (w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Search " + r.URL.String())
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	keys, _ := r.URL.Query()["keywords"]
+	fmt.Println("Received query for " + keys[0])
+
+	viewModel := models.ResultView{}
+	viewModel.Query = keys[0]
+
+	indexer.ForEachDocument(func(p *models.Document, i int) {
+		viewModel.Results = append(viewModel.Results, p)
+	})
+
+	resultTemplate.Execute(w, viewModel)
 }
 
-func faviconHandler (w http.ResponseWriter, r *http.Request) {
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func main(){
-	// Handle requests for static files (CSS, JS)
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static", http.StripPrefix("/static/", fs))
+func main() {
+	// Load indexer
+	indexer, _ = database.LoadIndexer("index.db")
+
+	// File servers
+	staticServer := http.FileServer(http.Dir("static"))
+	viewServer := http.FileServer(http.Dir("views"))
 
 	// Handle requests for routes
 	http.HandleFunc("/", helloWorldHandler)
-	http.HandleFunc("/search", searchHandler)
+	http.Handle("/views/", http.StripPrefix("/views/", viewServer))
+	http.Handle("/static/", http.StripPrefix("/static/", staticServer))
+	http.HandleFunc("/search/", searchHandler)
 	http.HandleFunc("/favicon.ico", faviconHandler)
+
 	fmt.Println(http.ListenAndServe(":80", nil))
 }

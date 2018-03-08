8606ea137412d5ebd331946963c242b3f94b3562
Dandi <rsmohamad@ust.hk>
2018-03-08 20:50:28 +0800

    Submit query as get request

--- cmd/server.go
+++ cmd/server.go
@@ -13,12 +13,22 @@ func helloWorldHandler (w http.ResponseWriter, r *http.Request){
 	fmt.Println(r.URL.String())
 }
 
+func searchHandler (w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Search " + r.URL.String())
+}
+
+func faviconHandler (w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main(){
 	// Handle requests for static files (CSS, JS)
 	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/static", http.StripPrefix("/static/", fs))
 
 	// Handle requests for routes
 	http.HandleFunc("/", helloWorldHandler)
+	http.HandleFunc("/search", searchHandler)
+	http.HandleFunc("/favicon.ico", faviconHandler)
 	fmt.Println(http.ListenAndServe(":80", nil))
 }
